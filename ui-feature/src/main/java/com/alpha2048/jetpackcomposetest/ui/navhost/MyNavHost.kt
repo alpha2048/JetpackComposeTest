cb9b769c73ab4686accfcef6f8a5904c3d6e520f
alpha2048 <alpha2048.jp@gmail.com>
2021-09-19 00:56:45 +0900

    fix library versions

--- ui-feature/src/main/java/com/alpha2048/jetpackcomposetest/ui/navhost/MyNavHost.kt
+++ ui-feature/src/main/java/com/alpha2048/jetpackcomposetest/ui/navhost/MyNavHost.kt
@@ -10,8 +10,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.alpha2048.jetpackcomposetest.ui.screen.RepositoryDetailScreen
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
